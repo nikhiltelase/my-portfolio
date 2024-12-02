fe7efa79cbdf272b4e0e737874af547ec09004ba
nikhil telase <nikhiltelase@gmail.com>
2024-12-02 22:41:47 +0530

    second commit '

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-if (process.env.NODE_ENV === 'production') {
-  // Enable service worker in production for offline use
-  import('./serviceWorker').then((module) => module.register());
-}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
