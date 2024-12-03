e0860c7328eede0a6a779a1c31e00302a8048ff4
nikhil telase <nikhiltelase@gmail.com>
2024-12-03 16:05:28 +0530

    edit vite config file"

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-export default {
-  base: './',
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Root base path for Netlify
+})
